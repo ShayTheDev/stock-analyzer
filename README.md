2cba351b288704cb8dffc65c9939e7af4f74dde6
ShayTheDev <shay_miller281@proton.me>
2026-06-26 21:25:55 +0300

    Implemented 'Clean Slate' on CSV

--- README.md
+++ README.md
@@ -10,5 +10,9 @@ and filter out systemic market panic from true, company specific signals.
 * [x] Implement core statistical metrics (mean, volatility, standard deviation) 
 
 ## Goals - June 20th 2026 
-* [] - Convert calculated metrics to plain English 
-* [] - Incorporate matplotlib(maybe) with the project
+* [x] - Convert calculated metrics to plain English 
+* [x] - Incorporate matplotlib(maybe) with the project 
+
+## Milestones - June 26th 2026 
+* [] - 'Clean Slate' ingestion pipeline: Correctly read the local CSV file so that each dtype is in its correct format. 
+* [] - Feature engineering & Clean target subsetting: solate the target columns you need for the analyzer (Close and Daily Return), calculate your risk metrics, and cleanly separate your data without triggering Pandas errors.
--- analyzer-v2.py
+++ analyzer-v2.py
@@ -4,10 +4,17 @@ import pandas as pd
 # Download Stock Data 
 # period_one = yf.download(tickers='S', start='2021-06-30', end='2023-06-30')
 # period_one_csv = period_one.to_csv('symbol_s_period_one')
-# %%
-period_one = pd.read_csv('symbol_s_period_one')
-period_one_np = period_one.to_numpy()
-print(period_one_np)
-period_one_df = pd.DataFrame(period_one_np) 
-# QuickView 'period_one' -----------------
+# %% "Clean Slate" Ingestion
+period_one = pd.read_csv(
+    'symbol_s_period_one',
+    header=[0,1],
+    index_col=0,
+    parse_dates=True)
+period_one.dropna(axis=0)
+period_one.columns = period_one.columns.get_level_values(0)
+period_one.columns.name = None
+period_one.index.name = 'Date'
+# %% QuickView 'period_one' -----------------
+period_one = period_one.round(2)
+print(period_one.describe())
 # %%
